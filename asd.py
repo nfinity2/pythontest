be7f34277feb870db78eb6a303c929e1ac42d95d
pleo <78929461+nfinity2@users.noreply.github.com>
2025-11-10 12:49:00 +0200

    harjutus01

---
+++ asd.py
@@ -0,0 +1,19 @@
+# Armin Cedric Allas
+# Harjutus 01
+# print("tere \"suur\" maailm!")
+import turtle
+
+turtle.forward(140)
+turtle.right(45)
+turtle.forward(50)
+turtle.backward(150)
+
+turtle.right(90)
+turtle.forward(150)
+turtle.done()
+
+
+
+
+
+
