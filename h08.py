a2b699adbc56f5c730274676b582d73a5ab035a2
pleo <armincedricallas@gmail.com>
2025-12-03 11:42:48 +0200

    Create h08.py

---
+++ h08.py
@@ -0,0 +1,69 @@
+# Armin Cedric Allas
+# Ülesanne 07 | 03.12.2025
+
+
+
+mootmine = ["jaanuar",-16,-12,-15,-20,0,-1,-20,-2,-20,-14,-18,-8,2,-1,-14,-7,-15,-17,-6,-17,-17,-7,0,3,-20,-17,-15,-8,-12,3]
+
+print(f"Mõõdetav kuu: {mootmine[0]}")
+print(f"Viimane mõõtmine: {mootmine[-1]}")
+print(f"Mõõtmised: {mootmine[1:]}")
+
+print(f"Suurim temp: {max(mootmine[1:])}")
+print(f"Suurim temp: {min(mootmine[1:])}")
+print(f"Keskmine: {sum(mootmine[1:])/len(mootmine[1:])}")
+print(f"-20 esineb: {mootmine[1:]. count(-20)}")
+
+
+#poolik
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+album = ['1. ALIKA – "Bridges"',
+         '2. Anett x Fredi – "Read Between The Lines"',
+         '3. villemdrillem "leekiv armastus"',
+         '4. Clicherik & Mäx – "PAKSUD"',
+        '5. nublu – "ära ärata"',
+         '6. NOËP – "Move Your Feet"',
+         '7. Trad.Attack! – "Bring It On"',
+         '8. Bedwetters – "It Is What It Is"',
+         '9. Reket – "Panama paberid"',
+         '10. Grete Paia – "Võluväel"']
+
+# print(album)
+# valik = int(input("Vali lugu: "))
+# print(album[valik-1])
+
+
