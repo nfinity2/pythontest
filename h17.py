fcea59e9ca0ff4084942c64411a1b58b24528a6c
pleo <armincedricallas@gmail.com>
2026-02-18 11:46:12 +0200

    e

--- h17.py
+++ h17.py
@@ -4,6 +4,11 @@ from datetime import date
 print("Tere", os.getlogin())
 print(os.getcwd())
 mitu = int(input("Mitu kataloogi tahad: "))
-today = date.today()
-print(today)
+today = str(date.today())
+try:
+    os.mkdir("uus_kaust")
+    for i in range(mitu):
+        os.mkdir(today+"/"+str(i+1))
+except FileExistsError:
+    print(f"Kataloog {today} juba eksisteerib.")
 
