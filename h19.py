3c57c0538758062e4b21dd748a367a7645145d8f
pleo <armincedricallas@gmail.com>
2026-02-19 10:00:53 +0200

    sssss

--- h19.py
+++ h19.py
@@ -10,9 +10,14 @@ with open(faili_nimi, mode='r', encoding='utf-8') as fail:
 
     print(f"Päise veerud: {pais}")
     for rida in csv_lugeja:
-        if (rida[1]) not in meeskonnad_kokku:
-            meeskonnad_kokku.append(rida[1])
-        if (rida[2]) not in meeskonnad_kokku:
-            meeskonnad_kokku.append(rida[2])
+        meeskonnad_kokku[rida[1]] = 0
+        # if (rida[1]) not in meeskonnad_kokku:
+        
 
-print(f"Meeskonnad kokku: {len(meeskonnad_kokku)}")
+            #meeskonnad_kokku.append(rida[1])
+        # if (rida[2]) not in meeskonnad_kokku:
+
+            #meeskonnad_kokku.append(rida[2])
+
+print(meeskonnad_kokku)
+# print(f"Meeskonnad kokku: {len(meeskonnad_kokku)}")
